f842adce3515e7495e63d8618df4ceb7295aeace
Barbara-RA <barbara.ramosalves94@gmail.com>
2024-08-24 19:09:47 -0300

    1° ajustes BenefitsSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import BenefitsSection from './components/Section/BenefitsSection/BenefitsSection'
 import RetentionSection from './components/Section/RetentionSection/RetentionSection'
 import './css/global.css'
 
 function App() {
   return (
     <>
+    <BenefitsSection/>
     <RetentionSection/>
     </>
   )
