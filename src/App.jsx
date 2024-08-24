883f51de08e0c0eff3d86c4ff4d9edb075a9cd5f
gcarloslima <carlosrelgs@gmail.com>
2024-08-24 17:18:24 -0300

    Organizando a estrutura de pastas

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import SpecialtySection from "./components/SpecialtySection/SpecialtySection"
-import Secao03 from "./components/Secao03/Secao03"
-import Whatsapp from "./components/Whatsapp/Whatsapp"
-import SectionDarkRed from "./components/SectionDarkRed/SectionDarkRed"
-import './css/style.css'
+import './css/global.css'
 
 function App() {
   return (
     <>
-      <SpecialtySection />
-      {/* <Secao03 />
-      <Whatsapp />
-      <SectionDarkRed /> */}
+
     </>
   )
 }
