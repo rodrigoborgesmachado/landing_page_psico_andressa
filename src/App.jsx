15470c457fd9c94b6487cf3b6e15d9bbad413e3d
diegohmade <diego.diniz@estudante.iftm.edu.br>
2024-08-25 19:37:21 -0300

    inclusao das seções na ordem em app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import Footer from './components/Footer/Footer'
 import BenefitsSection from './components/Section/BenefitsSection/BenefitsSection'
+import FAQSection from './components/Section/FAQSection/FAQSection'
+import MediaSection from './components/Section/MediaSection/MediaSection'
 import RetentionSection from './components/Section/RetentionSection/RetentionSection'
 import SpecialtySection from './components/Section/SpecialtySection/SpecialtySection'
 
@@ -11,6 +14,9 @@ function App() {
     <SpecialtySection/>
     <BenefitsSection/>
     <RetentionSection/>
+    <FAQSection/>
+    <MediaSection/>
+    <Footer/>
     </>
   )
 }
