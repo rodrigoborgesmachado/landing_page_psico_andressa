17bedce8d820345fd49efc347bf1a6752129ca0d
gcarloslima <carlosrelgs@gmail.com>
2024-08-28 18:50:20 -0300

    Ajustando margem e padding das seções

--- src/components/Common/ContentBlock.jsx
+++ src/components/Common/ContentBlock.jsx
@@ -1,8 +1,8 @@
 import styles from './css/Common.module.css';
 
-function ContentBlock({title, description}) {
+function ContentBlock({title, description, style: variant}) {
     return (
-        <div className={styles.contentBlock}>
+        <div className={`${styles.contentBlock} ${styles[variant]}`}>
             <h3>{title}</h3>
             <p>{description}</p>
         </div>
--- src/components/Section/AboutMeSection/AboutMeSection.jsx
+++ src/components/Section/AboutMeSection/AboutMeSection.jsx
@@ -15,23 +15,23 @@ function AboutMeSection() {
     return (
         <section className={styles.aboutMeSection}>
             <div>
-                <Title title={'Sobre Minha Jornada'}/>
+                <Title title={'Sobre Minha Jornada'} />
                 <div>
                     <div>
-                        <ContentBlock title={boxTopicos[0].title} description={boxTopicos[0].description} />
-                        <ContentBlock title={boxTopicos[1].title} description={boxTopicos[1].description} />
+                        <ContentBlock title={boxTopicos[0].title} description={boxTopicos[0].description} style="aboutMeContent" />
+                        <ContentBlock title={boxTopicos[1].title} description={boxTopicos[1].description} style="aboutMeContent" />
                     </div>
                     <div className={styles.boxImages}>
                         <img src="images/andressa1.svg" alt="Foto da Psicolaga Andressa em um auditório" />
                         <img src="images/andressa2.svg" alt="Foto da Psicolaga Andressa" />
                     </div>
                     <div>
-                    <ContentBlock title={boxTopicos[2].title} description={boxTopicos[2].description} />
-                    <ContentBlock title={boxTopicos[3].title} description={boxTopicos[3].description} />
+                        <ContentBlock title={boxTopicos[2].title} description={boxTopicos[2].description} style="aboutMeContent" />
+                        <ContentBlock title={boxTopicos[3].title} description={boxTopicos[3].description} style="aboutMeContent" />
                     </div>
                 </div>
                 <div className={styles.centralizedContent}>
-                <ContentBlock title={boxTopicos[4].title} description={boxTopicos[4].description} />
+                    <ContentBlock title={boxTopicos[4].title} description={boxTopicos[4].description} style="aboutMeContent" />
                 </div>
             </div>
         </section>
