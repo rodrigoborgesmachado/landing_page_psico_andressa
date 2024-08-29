9c35b7d8f1ecbe72170e94732d2f80000d10a167
Barbara-RA <barbara.ramosalves94@gmail.com>
2024-08-29 12:17:06 -0300

    Alteração da orderm de tópico AboutMe

--- src/components/Section/AboutMeSection/AboutMeSection.jsx
+++ src/components/Section/AboutMeSection/AboutMeSection.jsx
@@ -5,8 +5,8 @@ import ContentBlock from '../../Common/ContentBlock';
 function AboutMeSection() {
     const boxTopicos = [
         { id: 0, title: "Formação Acadêmica", description: "Graduada em Psicologia e pós-graduada em Psicologia Analítica de Carl Gustav Jung. Invisto constantemente em cursos que abordam a cura do feminino, maternidade, autoconhecimento e relacionamentos." },
-        { id: 1, title: "Abordagem Terapêutica", description: "Combino a psicoterapia com a psicoeducação, utilizando as teorias da Análise Transacional, Psicologia Analítica e Psicanálise, para atuar no desenvolvimento da autonomia do paciente." },
-        { id: 2, title: "Valores e Missão", description: "Comprometida com a formação contínua, ajudo minhas clientes a desenvolver autoestima através de um profundo processo de autoconhecimento, com o objetivo de alcançar saúde integral, autonomia e relacionamentos mais saudáveis." },
+        { id: 1, title: "Valores e Missão", description: "Comprometida com a formação contínua, ajudo minhas clientes a desenvolver autoestima através de um profundo processo de autoconhecimento, com o objetivo de alcançar saúde integral, autonomia e relacionamentos mais saudáveis." },
+        { id: 2, title: "Abordagem Terapêutica", description: "Combino a psicoterapia com a psicoeducação, utilizando as teorias da Análise Transacional, Psicologia Analítica e Psicanálise, para atuar no desenvolvimento da autonomia do paciente." },
         { id: 3, title: "Diferenciais", description: "Trabalho com uma abordagem dinâmica e personalizada, buscando desenvolver a sua autonomia e focando em um processo fluido, com cada vez menos dependência da psicoterapia para compreender conflitos internos e externos." },
         { id: 4, title: "Experiência", description: "Ministrei palestras e treinamentos na área da saúde, além de atuar com Terapia Sistêmica, o que me possibilitou trabalhar com dinâmicas e padrões familiares." }
     ]
