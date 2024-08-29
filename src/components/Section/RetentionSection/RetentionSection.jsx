f86012b607f3a9c3f44c590825439013eb5f4039
Diego Martins <diego.diniz@estudante.iftm.edu.br>
2024-08-28 21:16:53 -0300

    botoes funcionando modal

--- src/components/Section/RetentionSection/RetentionSection.jsx
+++ src/components/Section/RetentionSection/RetentionSection.jsx
@@ -1,8 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import Button from '../../Common/Button';
 import Title from '../../Common/Title';
+import ModalFormSection from '../ModalFormSection/ModalFormSection';
 import styles from './RetentionSection.module.css';
 
 function RetentionSection() {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+    useEffect(() => {
+        if (isModalOpen) {
+            document.body.classList.add('no-scroll');
+        } else {
+            document.body.classList.remove('no-scroll');
+        }
+    }, [isModalOpen]);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
 	return (
 		<section className={styles.retentionSection}>
 			<div>
@@ -14,9 +33,8 @@ function RetentionSection() {
 					<br />
 					Aprenda a se comunicar de forma assertiva, trabalhe conflitos internos e em seus relacionamentos, e encontre mais sentido e qualidade de vida. O processo terapêutico oferece um espaço acolhedor e seguro para você se conhecer melhor e crescer. Vamos começar essa jornada juntas?
 				</p>
-				<form action="">
-					<Button name="Entre em contato por e-mail" style="retentionSectionButton" />
-				</form>
+					<Button name="Entre em contato por e-mail" style="retentionSectionButton" onClick={openModal} />
+					<ModalFormSection isOpen={isModalOpen} onClose={closeModal} />
 				<div className={`${styles.flower} ${styles['flower-bottom-left']}`}></div>
 				<div className={`${styles.flower} ${styles['flower-top-right']}`}></div>
 			</div>
