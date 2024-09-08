0e8ec9728440d68bd8c829933abe6cfc3b3444be
Diego Martins <diego.diniz@estudante.iftm.edu.br>
2024-09-08 17:53:47 -0300

    responsividade modal

--- src/components/Section/ModalFormSection/ModalOverlay.jsx
+++ src/components/Section/ModalFormSection/ModalOverlay.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import ModalContent from './ModalContent';
-import styles from './ModalFormSection.module.css';
+    import React from 'react';
+    import ModalContent from './ModalContent';
+    import styles from './ModalFormSection.module.css';
 
-function ModalOverlay({ onClose }) {
-    return (
-        <div className={styles.modalOverlay}>
-            <div className={styles.modalContainer}>
-                <button className={styles.closeButton} onClick={onClose}>
-                    &times;
-                </button>
-                <ModalContent />
+    function ModalOverlay({ onClose }) {
+        return (
+            <div className={styles.modalOverlay}>
+                <div className={styles.modalContainer}>
+                    <button className={styles.closeButton} onClick={onClose}>
+                        &times;
+                    </button>
+                    <ModalContent />
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 
-export default ModalOverlay;
+    export default ModalOverlay;
