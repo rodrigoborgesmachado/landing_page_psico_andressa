18c37d033ff9b17bf7ee416b050b84600d733798
Barbara-RA <barbara.ramosalves94@gmail.com>
2024-09-08 12:58:44 -0300

    Efeitos de Emtrada RetentionSection

--- src/components/Section/RetentionSection/RetentionSection.jsx
+++ src/components/Section/RetentionSection/RetentionSection.jsx
@@ -1,11 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Button from '../../Common/Button';
 import Title from '../../Common/Title';
 import ModalFormSection from '../ModalFormSection/ModalFormSection';
 import styles from './RetentionSection.module.css';
+import 'animate.css';
 
 function RetentionSection() {
-	const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+    const sectionRef = useRef(null); // Ref para a seção que será observada
 
     useEffect(() => {
         if (isModalOpen) {
@@ -15,6 +18,33 @@ function RetentionSection() {
         }
     }, [isModalOpen]);
 
+    useEffect(() => {
+        // Cria o IntersectionObserver
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        // Exibir quando estiver visível e aplicar a animação
+                        setIsVisible(true);
+                    }
+                });
+            },
+            {
+                threshold: 0.05, // 5% da seção visível
+            }
+        );
+
+        if (sectionRef.current) {
+            observer.observe(sectionRef.current); // Observa a div
+        }
+
+        return () => {
+            if (sectionRef.current) {
+                observer.unobserve(sectionRef.current); // Limpa o observador quando o componente desmonta
+            }
+        };
+    }, []);
+
     const openModal = () => {
         setIsModalOpen(true);
     };
@@ -22,24 +52,30 @@ function RetentionSection() {
     const closeModal = () => {
         setIsModalOpen(false);
     };
-	return (
-		<section className={styles.retentionSection}>
-			<div>
-				<Title title={'Descubra Seu Potencial!'} style={'titleRetentionSection'}/>
-				<p>
-					<span className={styles.firstLine}>
-						Você pode descobrir sua essência e desenvolver seu potencial reconstruindo sua autoestima.
-					</span>
-					<br />
-					Aprenda a se comunicar de forma assertiva, trabalhe conflitos internos e em seus relacionamentos, e encontre mais sentido e qualidade de vida. O processo terapêutico oferece um espaço acolhedor e seguro para você se conhecer melhor e crescer. Vamos começar essa jornada juntas?
-				</p>
-					<Button name="Entre em contato por e-mail" style="retentionSectionButton" onClick={openModal} />
-					<ModalFormSection isOpen={isModalOpen} onClose={closeModal} />
-				<div className={`${styles.flower} ${styles['flower-bottom-left']}`}></div>
-				<div className={`${styles.flower} ${styles['flower-top-right']}`}></div>
-			</div>
-		</section>
-	);
+
+    return (
+        <section className={styles.retentionSection}>
+            <div
+                ref={sectionRef} // Associa o ref à div
+                // Usa `opacity: 0` até que o conteúdo seja visível
+                className={`${isVisible ? 'animate__animated animate__fadeInUp' : ''}`}
+                style={{ opacity: isVisible ? 1 : 0, transition: 'opacity 0.5s ease-in-out' }}
+            >
+                <Title title={'Descubra Seu Potencial!'} style={'titleRetentionSection'} />
+                <p>
+                    <span className={styles.firstLine}>
+                        Você pode descobrir sua essência e desenvolver seu potencial reconstruindo sua autoestima.
+                    </span>
+                    <br />
+                    Aprenda a se comunicar de forma assertiva, trabalhe conflitos internos e em seus relacionamentos, e encontre mais sentido e qualidade de vida. O processo terapêutico oferece um espaço acolhedor e seguro para você se conhecer melhor e crescer. Vamos começar essa jornada juntas?
+                </p>
+                <Button name="Entre em contato por e-mail" style="retentionSectionButton" onClick={openModal} />
+                <div className={`${styles.flower} ${styles['flower-bottom-left']}`}></div>
+                <div className={`${styles.flower} ${styles['flower-top-right']}`}></div>
+            </div>
+            <ModalFormSection isOpen={isModalOpen} onClose={closeModal} />
+        </section>
+    );
 }
 
 export default RetentionSection;
