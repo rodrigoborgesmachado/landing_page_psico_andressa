9e43ccdd199fb0632c362ba14b46c70622f6f2dc
Barbara-RA <barbara.ramosalves94@gmail.com>
2024-09-11 00:11:41 -0300

    Seta de indicação para baixo

--- src/components/Section/HomeSection/HomeSection.jsx
+++ src/components/Section/HomeSection/HomeSection.jsx
@@ -1,12 +1,36 @@
+import React, { useEffect, useState } from 'react';
 import HomeContent from "./HomeContent";
 import styles from "./HomeSection.module.css";
 
 function HomeSection() {
+    const [isVisible, setIsVisible] = useState(true);
+
+    // Função para detectar quando o usuário está na última seção
+    useEffect(() => {
+        const handleScroll = () => {
+            const lastSection = document.getElementById('last-section');
+            const bottomOfLastSection = lastSection?.getBoundingClientRect().top < window.innerHeight;
+            
+            // Se estiver na última seção, esconde a seta
+            setIsVisible(!bottomOfLastSection);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
         <section className={styles.homeSection} id={"home"}>
             <div>
                 <HomeContent />
             </div>
+            {isVisible && (
+                <img
+                    src="/icons/Seta_paraBaixo.png"
+                    alt="Seta para baixo"
+                    className={styles.arrowIcon} // Classe específica para o ícone
+                />
+            )}
         </section>
     );
 }
