61f84b997b1b8b0f3464f960d00249627dd59604
Rodrigo Machado <rodrigoborgesmachado@gmail.com>
2024-09-16 12:21:09 -0300

    Adicionando loading.

--- src/components/Section/HomeSection/HomeSection.jsx
+++ src/components/Section/HomeSection/HomeSection.jsx
@@ -3,9 +3,12 @@ import Carousel from "react-responsive-carousel/lib/js/components/Carousel/index
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import HomeContent from "./HomeContent";
 import styles from "./HomeSection.module.css";
+import ModalWaiting from '../ModalFormSection/ModalWaiting';
 
 function HomeSection() {
     const [isVisible, setIsVisible] = useState(true);
+    const [imagesLoaded, setImagesLoaded] = useState(1);
+    const [allImagesLoaded, setAllImagesLoaded] = useState(false);
 
     const images = [
         'Home_Terapia.svg',
@@ -27,56 +30,74 @@ function HomeSection() {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
-    return (
-        <section className={styles.homeSection} id={"home"}>
-            <div>
-                <HomeContent/>
-            </div>
-            {isVisible && (
-                <img
-                    src="/icons/Seta_paraBaixo.png"
-                    alt="Seta para baixo"
-                    className={styles.arrowIcon} // Classe específica para o ícone
-                />
-            )}
-            <span className={styles.carousel}>
-              <Carousel
-                  autoPlay
-                  infiniteLoop
-                  showThumbs={false}
-                  showStatus={false}
-                  showArrows={false}
-                  swipeable={false}
-                  interval={6000}
-                  transitionTime={800}
-                  animationHandler="fade"
-                  showIndicators={false}
-                  stopOnHover={false}
-                  dynamicHeight={false}
-                  emulateTouch={false}
-              >
+    useEffect(() => {
+        if (imagesLoaded >= images.length) {
+            setAllImagesLoaded(true);
+        }
+    }, [imagesLoaded]);
 
-                {images.map((image, index) => (
-                <React.Fragment key={index}>
-                    <div key={`desktop-${index}`} style={{height: '100vh'}} className={styles.carouselDesktop}>
+    const handleImageLoad = () => {
+        setImagesLoaded(prev => prev + 1);
+    };
+
+    return (
+        <>
+            {
+                !allImagesLoaded && <ModalWaiting/>
+            }
+            <section  className={styles.homeSection} id={"home"}>
+                <div>
+                    <HomeContent/>
+                </div>
+                {
+                    isVisible && (
                     <img
-                        src={`https://questoesaqui.blob.core.windows.net/psicodede/${image}`}
-                        alt={`Slide ${index + 1}`}
-                        style={{objectFit: 'cover', height: '100%', width: '100%'}}
+                        src="/icons/Seta_paraBaixo.png"
+                        alt="Seta para baixo"
+                        className={styles.arrowIcon} // Classe específica para o ícone
                     />
-                    </div>
-                    <div key={`mobile-${index}`} style={{height: '100vh'}} className={styles.carouselMobile}>
-                    <img
-                        src={`https://questoesaqui.blob.core.windows.net/psicodede/Mobile_${image}`}
-                        alt={`Slide ${index + 1}`}
-                        style={{objectFit: 'cover', height: '100%', width: '100%'}}
-                    />
-                    </div>
-                </React.Fragment>
-                ))}
-            </Carousel>
-            </span>
-        </section>
+                )}
+                <span className={styles.carousel}>
+                <Carousel
+                    autoPlay
+                    infiniteLoop
+                    showThumbs={false}
+                    showStatus={false}
+                    showArrows={false}
+                    swipeable={false}
+                    interval={6000}
+                    transitionTime={800}
+                    animationHandler="fade"
+                    showIndicators={false}
+                    stopOnHover={false}
+                    dynamicHeight={false}
+                    emulateTouch={false}
+                >
+
+                    {images.map((image, index) => (
+                    <React.Fragment key={index}>
+                        <div key={`desktop-${index}`} style={{height: '100vh'}} className={styles.carouselDesktop}>
+                        <img
+                            src={`https://questoesaqui.blob.core.windows.net/psicodede/${image}`}
+                            alt={`Slide ${index + 1}`}
+                            style={{objectFit: 'cover', height: '100%', width: '100%'}}
+                            onLoad={handleImageLoad}
+                        />
+                        </div>
+                        <div key={`mobile-${index}`} style={{height: '100vh'}} className={styles.carouselMobile}>
+                        <img
+                            src={`https://questoesaqui.blob.core.windows.net/psicodede/Mobile_${image}`}
+                            alt={`Slide ${index + 1}`}
+                            style={{objectFit: 'cover', height: '100%', width: '100%'}}
+                        />
+                        </div>
+                    </React.Fragment>
+                    ))}
+                </Carousel>
+                </span>
+                
+            </section>
+        </>
     );
 }
 
